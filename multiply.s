01e0212d8a57eb6deae3fb1ee385000cc33ecd40
GrossTrevor <grosstrevor000@gmail.com>
2023-09-28 17:32:47 -0400

    final commit

--- multiply.s
+++ multiply.s
@@ -9,15 +9,15 @@ result		:	.asciz	"x * y = %d\n"
 
 .global main
 
-#main
+# main
 main:
 
-#create room on stack for x
+# create room on stack for x and y
 sub sp, sp, 8
 # input x prompt
 ldr x0, = input_x_prompt
 bl printf	
-#get input x value
+# get input x value
 # spec input
 ldr x0, = input_spec
 mov x1, sp
@@ -25,17 +25,20 @@ bl scanf
 ldrsw x19, [sp]
 
 
-#get y input value
-# enter y output
+# input y prompt
 ldr x0, = input_y_prompt
 bl printf
+# get input y value
 # spec input
 ldr x0, = input_spec
 mov x1, sp
 bl scanf
 ldrsw x20, [sp]
 
-#multiply x and y
+# set x21 to 0
+mov x21, 0
+
+# multiply x and y
 recmul:
 	cmp x20, 0
 	b.eq exit
@@ -43,14 +46,11 @@ recmul:
 	sub x20, x20, 1
 	b recmul
 
-#old add funtionality
-#add x21, x19, x20
-#mov x1, x21
-#ldr x0, =result
-#bl printf
-
 # branch to this label on program completion
 exit:
+	mov x1, x21
+	ldr x0, =result
+	bl printf
 	mov x0, 0
 	mov x8, 93
 	svc 0
